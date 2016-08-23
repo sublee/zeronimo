7d006e878816495580b274628bece9536b5846ff
Heungsub Lee <sub@subl.ee>
2016-08-23 18:24:36 +0900

    ci

--- test.py
+++ test.py
@@ -286,12 +286,12 @@ def test_slow(worker, collector, push):
             print 4
             customer.call('sleep', 0.3).get()
             print 5
-        t = time.time()
-        print 6
-        assert customer.call('sleep', 0.1).get() == 0.1
-        print 7
-        assert time.time() - t >= 0.1
-        print 8
+    print 6
+    t = time.time()
+    print 7
+    assert customer.call('sleep', 0.1).get() == 0.1
+    print 8
+    assert time.time() - t >= 0.1
     print 9
 
 
